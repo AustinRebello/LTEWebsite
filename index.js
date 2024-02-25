6de4b5bf58d902609d0b0391cc6600c808d9c41b
Austin Rebello <55983501+AustinRebello@users.noreply.github.com>
2024-02-24 19:01:22 -0500

    Inital Project Setup
    Added several scaffolds for different races and the news, as well as basic devise stuff, need to incorporate later. Worked a bit on the home page, awaiting actual graphics before flushing out full design.

--- index.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    fetch(' https://scrape.spoticord.workers.dev/?url=https://static01.nyt.com/elections-assets/pages/data/2024-02-13/results-new-york-us-house-3-special.json') 
-    .then(response => response.json()) 
-    .then(data => console.log(data)) 
-    .catch(error => console.error(error)  );
-});
