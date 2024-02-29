ab69cfe41c98c9aeacf4c7243b62fcf0aa6ea4f5
drewgifford <drew@drewgifford.com>
2024-02-29 16:47:37 -0500

    drew branch!

--- db/migrate/20240229214551_add_role_to_user.rb
+++ db/migrate/20240229214551_add_role_to_user.rb
@@ -1,5 +1,6 @@
 class AddRoleToUser < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :admin, :boolean, default:false
+    add_column :users, :super_user, :boolean, default:false
   end
 end
