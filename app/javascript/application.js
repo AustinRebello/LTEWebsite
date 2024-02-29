d768cd7d89c261eef5bc4cc73af294bc7efb5548
Austin Rebello <55983501+AustinRebello@users.noreply.github.com>
2024-02-29 18:11:31 -0500

    First attempt at dynamic state colorings for race leans

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,51 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 console.log("Hello World APPLICATIONS");
+
+
+function determineFillCounties(democrat, republican) {
+    gap = democrat-republican;
+    if(gap >= 15){
+        return "#0047FF";
+    } else if (gap >= 7.5){
+        return "#5F85FF";
+    } else if (gap >= 3){
+        return "#9FB5FF";
+    } else if (gap > 0){
+        return "#B2B8D1";
+    } else if (gap = 0){
+        return "#8f96a3";
+    } else if (gap > -3){
+        return "#CFAAA2";
+    } else if (gap > -7.5){
+        return "#FFA9B6";
+    } else if (gap > -15){
+        return "#FF7392";
+    } else {
+        return "#FF0052";
+    }
+};
+
+function determineFillRace(democrat, republican, called) {
+    gap = democrat-republican;
+    console.log("Hello World APPLICATIONS");
+    if(gap >= 15){
+        return "#0047FF";
+    } else if (gap >= 7.5){
+        return "#5F85FF";
+    } else if (gap >= 3){
+        return "#9FB5FF";
+    } else if (gap > 0){
+        return "#B2B8D1";
+    } else if (gap = 0){
+        return "#8f96a3";
+    } else if (gap > -3){
+        return "#CFAAA2";
+    } else if (gap > -7.5){
+        return "#FFA9B6";
+    } else if (gap > -15){
+        return "#FF7392";
+    } else {
+        return "#FF0052";
+    };
+};
